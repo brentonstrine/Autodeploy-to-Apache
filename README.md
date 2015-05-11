e629d4e338e7a50432dc8bc42853f56571762da2
Brenton Strine <brenton@appleleaf.attlocal.net>
2015-05-10 17:37:14 -0700

    Forking from JPL GHE

---
+++ README.md
@@ -0,0 +1,56 @@
+##Set-up Instructions
+
+Generally, the steps to setting up are:
+
+1. [Generate an SSH key on NextGen](#1-generate-an-ssh-key-on-nextgen)
+2. [Add the key to GitHub Enterprise](#2-add-the-key-to-github-enterprise)
+3. [Copy `autodeploy.php` to your web root](#3-copy-autodeploy-to-your-web-root)
+4. [Configure autodeploy](#4-configure-autodeploy)
+5. [Set up webhooks in GitHub](#5-set-up-webhooks-in-github)
+
+###1. Generate an SSH key on NextGen
+A general tutorial for generating SSH keys can be found [here](https://help.github.com/articles/generating-ssh-keys/).
+
+What follows is specific to JPL and assumes you're installing on NextGen and using JPL's GitHub Enterprise. 
+
+Open a terminal and type the following commands (replacing terms in ALLCAPS with the appropriate values):
+
+```ssh
+ ssh YOURUSERNAME@webhosting-login01.jpl.nasa.gov
+ sudo become YOURSITEHOSTNAME
+ ssh-keygen -t rsa -C "YOURUSERNAME@jpl.nasa.gov"
+```
+ The passphrase *must* be blank. Type `enter` twice to set it as blank.
+```ssh
+ cd /websites/YOURSITEHOSTNAME/private/.ssh
+ vi id_rsa.pub
+```
+Copy the contents of the file into a temporary text file. To exit the `vi` editor, type:
+```ssh
+ :exit
+```
+###2. Add the key to GitHub Enterprise
+Log in to GitHub Enterprise and go to Settings > SSH Keys > Add SSH Key. Paste the new key in. Back in terminal again type to test:
+```ssh
+ssh -T git@github.jpl.nasa.gov
+```
+If you did everything right, it should output something like this, or possibly only the last line:
+```
+The authenticity of host 'github.jpl.nasa.gov (128.149.186.45)' can't be established.
+RSA key fingerprint is a5:e4:e5:d5:be:f4:6b:a3:d2:09:50:5a:d3:a6:0f:b7.
+Are you sure you want to continue connecting (yes/no)? yes
+Warning: Permanently added 'github.jpl.nasa.gov,128.149.186.45' (RSA) to the list of known hosts.
+Hi YOURUSERNAME! You've successfully authenticated, but GitHub does not provide shell access.
+```
+If not, something is wrong, do not pass GO, do not continue until you've figured out what's going on. 
+
+###3. Copy autodeploy to your web root
+Copy `autodeploy.php` from this repo into your web root. 
+
+###4. Configure autodeploy
+Open `autodeploy.php` and edit the variables in the top.
+
+###5. Set up webhooks in GitHub
+Go to GitHub > YOURPROJECT > Settings > Webhooks & Services > Payload URL. Insert the URL to `autodeploy.php`.
+
+Now `push` a change to `master` on GitHub and see it automagically appear on your NextGen server!
---
+++ autodeploy.php
@@ -0,0 +1,120 @@
+<!doctype html><html><head><title>AutoDeploy Script</title></head></head><body><?php // This script created entirely by Brenton Strine. GPL 3.0 License.
+
+// Assumptions:
+// 1. You've created a SSH key and put it in GitHub (https://help.github.com/articles/generating-ssh-keys/)
+// 2. You've placed this script into the web root of your website on the NextGen server.
+// 3. You've configured the items below
+// 4. You've set up a web hook in GitHub so that this script runs every time the branch you want to
+
+$deploy_location  = ".";         // Location you want to deploy to. Usually will be "." (web root). For a subdirectory do "subdir" or "subdir/furthersubdir".
+$repo             = "YOUR-REPO"; // Name of the  repo  you're deploying from. Must be exact GitHub repo name.
+$branch           = "master";    // Name of the branch you're deploying from. Usually will be "master" or name of some other branch.
+$folder           = "www";       // Name of a folder within the repo you want to copy. Put "" to copy the entire repo. To only copy from a particular subdir put the path to it, e.g. "www" or "site/production"
+$notify           =              // List of emails to send notification to when this script is run
+    ["YOURNAME@jpl.nasa.gov"];
+$account          = "YOURTEAM";  // The GitHub account name or team name
+$githubDomain     = "github.jpl.nasa.gov";// Will be "github.com" unless you're using GitHub Enterprise
+
+
+/* ------------------------------------------------------------------------------- */
+/* ---------------- no need to configure anything below this line ---------------- */
+/* ------------------------------------------------------------------------------- */
+
+// update repo. if it's not there, try to create it.
+if(is_dir("../private/autodeploy") && is_dir("../private/autodeploy/.git")){
+    $git_output = shell_exec("cd ../private/autodeploy && git pull git@".$githubDomain.":" . $account . "/" . $repo . " " . $branch . "  2>&1");
+} else {
+    echo "<h3>AutoDeploy Configuration</h3><p>Let's try to clone the repo into <code>private/autodeploy</code>.</p>";
+    echo "<pre class='code'>";
+    echo "<b>git clone git@".$githubDomain.":" . $account . "/" . $repo . " autodeploy</b><br>";
+    echo $git_output = shell_exec("cd ../private && git clone git@".$githubDomain.":" . $account . "/" . $repo . " autodeploy 2>&1");
+    echo"</pre>";
+    if(strpos($git_output, "fatal") >= 0 && (strpos($git_output, "private mode is enabled") || strpos($git_output, "Host key verification failed") || strpos($git_output, "Permission denied") ) ){
+        echo "<p>Oops. Looks like a SSH key isn't set up! Please follow instructions <a href='https://help.github.com/articles/generating-ssh-keys/'>on this page</a> to set up SSH keys for this site and then plug them into GitHub.</p>";
+    } else {
+        echo "<p>Cool, looks like the repo was cloned! Try reloading and we'll see if this works.</p>";
+    }
+}
+// vars for copying function
+$total_copied = 0;
+$total_files = 0;
+$copied_files = "";
+
+// copy files from repo to destination directory
+recurse_copy_newer("../private/autodeploy/" . $folder, $deploy_location);
+
+// copy all contents of a directory to the destination as long as the source file is newer than the destination file
+function recurse_copy_newer($src,$dst) {
+    global $total_copied, $total_files, $copied_files;
+    $dir = opendir($src);
+    @mkdir($dst);
+    while(($file = readdir($dir)) !== false) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) ) {
+                recurse_copy_newer($src . '/' . $file,$dst . '/' . $file);
+            } else {
+                $total_files++;
+                $from = $src . '/' . $file;
+                $to = $dst . '/' . $file;
+                if(filemtime($from) > filemtime($to)){
+                    copy($from,$to);
+                    $total_copied++;
+                    $copied_files .= $to . "<br>";
+                }
+            }
+        }
+    }
+    closedir($dir);
+}
+
+// build notification info for email or manual execution of this script
+$hostName       = $_SERVER[SERVER_NAME];
+$scriptLocation = $_SERVER[SCRIPT_FILENAME];
+$sourcePath = "http://$githubDomain/$account/$repo/tree/$branch/$folder";
+$copy_to = ($copy_to==".") ? "" : $copy_to;
+$percent_copied = ($total_copied / $total_files) * 100;
+$headers  = "MIME-Version: 1.0\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Transfer-Encoding: 7bit\r\n";
+$subject = "AutoDeploy from repo $account/" . $repo . " to http://" . $hostName;
+$email_body = "A total of <b>$total_copied files were copied</b> from <a href='$sourcePath'>GitHub</a> to <a href='http://$hostName'>$hostName</a>.</p>";
+$email_body .= "<p>First, this script did a <code>git pull</code> on branch <code>$branch</code> of <code>$repo</code> GitHub responded:</p>";
+$email_body .= "<pre class='code'>$git_output</pre>";
+$email_body .= "<p>Then this script copied all new files from <a href='$sourcePath'><code>$sourcePath</code></a> to <a href='$hostName'><code>http://" . $hostName  . "/</code></a>.</p>";
+if($percent_copied>0){
+    $email_body .= "<p>The following $total_copied files (" . intval($percent_copied) . "% of the repo) were updated.</p>";
+    $email_body .= "<pre class='code'>" . $copied_files . "</pre>";
+} else {
+    $email_body .= "<p><strong>No files were copied</strong>. All files were already present on the server and up to date.</p>";
+}
+$email_body .= "<hr><p><small>This AutoDeploy script is located at <code>$scriptLocation</code>.</small></p>";
+$email_body .= "<style>code,.code {background-color:#9BD;border:dotted 1px grey;border-radius:1px;padding: 1px 2px;}pre.code{overflow:auto;background-color:#88AECD;padding:1em;margin:0 1em;}</style>";
+
+// send notification email
+mail(rtrim(implode(',', $notify), ','), $subject, $email_body, $headers);
+
+// output info for manual exexution of this scirpt
+echo "<h3>AutoDeploy from repo <code>$sourcePath</code> to <code>http://" . $hostName . "</code></h3><hr>";
+echo $email_body; /* */
+?>
+<style scoped>
+    body{
+        background-color: #333;
+        color: #ddd;
+        font-family: sans-serif;
+        font-size: 1.5em;
+        max-width: 50em;
+        margin: 1em auto;
+        font-weight: 100;
+    }
+    a{color:#9bf;}
+    b,strong{font-weight: 400;color:#fff;}
+    code,pre.code{background-color:rgba(136, 174, 205, .2);}
+    code{background-color: #303030;}
+    pre b{
+        display: block;
+        background-color: rgba(0, 0, 0, .2);
+        margin: -1em -1em 0 -1em;
+        padding: 0.2em 1em;
+        border-bottom: dotted 1px #666;
+    }
+</style>
+</body></html>
